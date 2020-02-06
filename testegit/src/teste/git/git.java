d13efc5fdf50c60aab71ef0b2e98b7cc9f56ebbe
Guilherme <57097641+inaba2@users.noreply.github.com>
2020-02-06 14:38:39 -0300

    add arq

---
+++ testegit/src/teste/git/git.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package teste.git;
+
+/**
+ * @author spi-droid
+ *
+ */
+public class git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("git teste");
+
+	}
+
+}
